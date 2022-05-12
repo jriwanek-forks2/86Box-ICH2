094404c1a83af43bfc40692f5ad02581c27a2d1f
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2022-05-12 14:23:55 +0300

    Add a discrete 486 board

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -29,6 +29,7 @@
 
 const machine_filter_t machine_types[] = {
     { "None",                      MACHINE_TYPE_NONE       },
+    { "AT",				           MACHINE_TYPE_AT  	   },
     { "SiS 471",				   MACHINE_TYPE_SIS_471	   },
     { "Intel ICH2",				   MACHINE_TYPE_ICH2	   }
 };
@@ -43,13 +44,17 @@ const machine_filter_t machine_chipsets[] = {
 const machine_t machines[] = {
 
     /* SiS 471 Based Machines */
-    { "[SiS 471] AOpen Vi15G",                         "vi15g",         MACHINE_TYPE_SIS_471,       MACHINE_CHIPSET_SIS_471,             machine_at_vi15g_init,            0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET3,                   CPU_BLOCK_NONE,        			            20000000, 50000000,  0, 0, 0, 0,           MACHINE_AT,        MACHINE_FLAGS_NONE,  8192, 131072,  8192, 255, NULL, NULL },
-    { "[SiS 471] ASUS VL/I-486SV2G",                   "486sv2g",       MACHINE_TYPE_SIS_471,       MACHINE_CHIPSET_SIS_471,             machine_at_486sv2g_init,          0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET3,                   CPU_BLOCK_NONE,        			            20000000, 50000000,  0, 0, 0, 0,           MACHINE_PS2_VLB,   MACHINE_FLAGS_NONE,  8192,  65536,  8192, 255, NULL, NULL },
+    { "[Intel 82330/82331] 1990 AT Board",             "at1990",        MACHINE_TYPE_AT,            MACHINE_CHIPSET_NONE,                machine_at_at1990_init,           0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET1,                   CPU_BLOCK_NONE,        			            0, 0, 0, 0, 0, 0,                          MACHINE_AT,        MACHINE_FLAGS_NONE,  1024,  16384,  1024, 255, NULL, NULL },
+
+    /* SiS 471 Based Machines */
+    { "[SiS 471] AOpen Vi15G",                         "vi15g",         MACHINE_TYPE_SIS_471,       MACHINE_CHIPSET_SIS_471,             machine_at_vi15g_init,            0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET3,                   CPU_BLOCK_NONE,        			            20000000, 50000000, 0, 0, 0, 0,            MACHINE_AT,        MACHINE_FLAGS_NONE,  8192, 131072,  8192, 255, NULL, NULL },
+    { "[SiS 471] ASUS VL/I-486SV2G",                   "486sv2g",       MACHINE_TYPE_SIS_471,       MACHINE_CHIPSET_SIS_471,             machine_at_486sv2g_init,          0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET3,                   CPU_BLOCK_NONE,        			            20000000, 50000000, 0, 0, 0, 0,            MACHINE_PS2_VLB,   MACHINE_FLAGS_NONE,  8192,  65536,  8192, 255, NULL, NULL },
 
     /* Intel 815EP Based Machines */
-    { "[Intel i815EP] ASUS CUSL2-C",                   "cusl2c",           MACHINE_TYPE_ICH2,       MACHINE_CHIPSET_INTEL_I815EP,        machine_at_cusl2c_init,           0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET370,                 CPU_BLOCK_NONE,        			            66666667, 133333333, 1300, 3500, 1.5, 8.0, MACHINE_PS2_NOISA,   MACHINE_IDE_DUAL, 32768, 524288, 32768, 255, NULL, NULL },
-    { "[Intel i815EP] Tyan Tomcat i815T",              "s2080",            MACHINE_TYPE_ICH2,       MACHINE_CHIPSET_INTEL_I815EP,        machine_at_s2080_init,            0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET370,                 CPU_BLOCK(CPU_CYRIX3S),        			    66666667, 133333333, 1300, 3500, 1.5, 8.0, MACHINE_PS2_NOISA,   MACHINE_IDE_DUAL, 32768, 524288, 32768, 255, NULL, NULL },
-    { NULL,                                            NULL,               MACHINE_TYPE_NONE,       MACHINE_CHIPSET_NONE,                NULL,                             0, 0, MACHINE_AVAILABLE, 0 , 0,                                 CPU_BLOCK_NONE,                              0, 0, 0, 0, 0, 0, MACHINE_BUS_NONE,  MACHINE_FLAGS_NONE, 0, 0, 0, 0, NULL, NULL }
+    { "[Intel i815EP] ASUS CUSL2-C",                   "cusl2c",        MACHINE_TYPE_ICH2,          MACHINE_CHIPSET_INTEL_I815EP,        machine_at_cusl2c_init,           0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET370,                 CPU_BLOCK_NONE,        			            66666667, 133333333, 1300, 3500, 1.5, 8.0, MACHINE_PS2_NOISA,   MACHINE_IDE_DUAL, 32768, 524288, 32768, 255, NULL, NULL },
+    { "[Intel i815EP] Tyan Tomcat i815T",              "s2080",         MACHINE_TYPE_ICH2,          MACHINE_CHIPSET_INTEL_I815EP,        machine_at_s2080_init,            0, 0, MACHINE_AVAILABLE, 0 , CPU_PKG_SOCKET370,                 CPU_BLOCK(CPU_CYRIX3S),        			    66666667, 133333333, 1300, 3500, 1.5, 8.0, MACHINE_PS2_NOISA,   MACHINE_IDE_DUAL, 32768, 524288, 32768, 255, NULL, NULL },
+
+    { NULL,                                            NULL,            MACHINE_TYPE_NONE,          MACHINE_CHIPSET_NONE,                NULL,                             0, 0, MACHINE_AVAILABLE, 0 , 0,                                 CPU_BLOCK_NONE,                              0, 0, 0, 0, 0, 0, MACHINE_BUS_NONE,  MACHINE_FLAGS_NONE, 0, 0, 0, 0, NULL, NULL }
 
 };
 
